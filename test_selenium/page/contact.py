d5bd149871e3bb4abe8414f63e000c17393f802f
MeiBin <812574402@qq.com>
2020-01-06 20:44:34 +0800

    最初的PO代码

---
+++ test_selenium/page/contact.py
@@ -0,0 +1,25 @@
+class Contact:
+    def add_member(self):
+        pass
+
+    def search(self):
+        pass
+
+    def import_users(self):
+        pass
+
+    def export_users(self):
+        pass
+
+    def set_department(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def invite(self):
+        pass
+
+    def add_department(self):
+        pass
+    
